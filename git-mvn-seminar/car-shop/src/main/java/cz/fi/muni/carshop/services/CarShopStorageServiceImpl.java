23a2fba1b3535d5a547eb71ddc5284d563f51dc2
Lenka Smitalova <Lenka@auth43-71.fi.muni.cz>
2017-10-18 13:15:19 +0200

    =git-mvn-feature -> Tacsk 03

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -29,7 +30,23 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 	}
 
 	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+		List<Car> carsOfSameType = allCars.get(car.getType());
+		carsOfSameType
+				.stream()
+				.filter( currentCar -> currentCar.equals(car))
+				.collect(Collectors.toList());
+		if (carsOfSameType.size() == 0) {
+			throw new RequestedCarNotFoundException("Car doesn't exist.");
+		}
+	}
+
+	@Override
 	public void addCarToStorage(Car car) {
+		if (car.getPrice() < 0) {
+			throw new IllegalArgumentException("Car price is less than 0!");
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
 
